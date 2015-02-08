3b5e6a05d7c0fa91e7c09cfbbb24c6d3eeb45a7b
Andy Reischle <andy@reischle.net>
2015-02-08 18:43:25 +0100

    changed sensor to gpio2

--- alarm-public.lua
+++ alarm-public.lua
@@ -6,10 +6,10 @@ SensorID = "1"
 status = "CLEAR"
 oldstatus = "CLEAR"
 
-
+gpio.mode(4,gpio.INPUT,gpio.FLOAT)
 
 tmr.alarm(0, 1000, 1, function() -- Set alarm to one second
-	if gpio.read(3)==1 then status="ALARM" else status="CLEAR" end
+	if gpio.read(4)==1 then status="ALARM" else status="CLEAR" end
     if status ~= oldstatus then sendalarm (SensorID,status) end
 	oldstatus = status
 end)
